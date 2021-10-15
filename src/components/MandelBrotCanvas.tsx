47f70b247fa269d7a0a42bc71616bd4ae925e0e0
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-10-15 12:54:31 +0200

    feat: :sparkles: navbar and madelbrotcanvas

--- src/components/MandelBrotCanvas.tsx
+++ src/components/MandelBrotCanvas.tsx
@@ -1,11 +1,13 @@
 import React, { useRef, FunctionComponent, useState, useEffect } from "react";
 import Shader from "../utils/shader";
 import { loadFile } from "../utils/webglutils";
+import NavbarComponent from "./NavbarComponent";
 
 interface MandelBrotCanvasProps {}
 
 const MandelBrotCanvas: FunctionComponent<MandelBrotCanvasProps> = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [GL, setGL] = useState<WebGL2RenderingContext>(null);
   const [shader, setShader] = useState<Shader>();
 
   useEffect(() => {
@@ -13,6 +15,10 @@ const MandelBrotCanvas: FunctionComponent<MandelBrotCanvasProps> = () => {
       if (canvasRef.current) {
         canvasRef.current.width = window.innerWidth;
         canvasRef.current.height = window.innerHeight;
+
+        if (GL) {
+          GL.viewport(0, 0, GL.canvas.width, GL.canvas.height);
+        }
       }
     };
 
@@ -30,26 +36,49 @@ const MandelBrotCanvas: FunctionComponent<MandelBrotCanvasProps> = () => {
         const s = new Shader(gl, vertSrc, fragSrc);
         setShader(s);
 
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
         gl.clearColor(0, 0, 0, 0);
         gl.clear(gl.COLOR_BUFFER_BIT);
+
+        setGL(gl);
       }
     };
 
     window.addEventListener("resize", updateCanvasSize);
+    window.addEventListener("resize", handleUpdate);
 
     updateCanvasSize();
     handleStart();
+    handleUpdate();
+
+    return () => {
+      window.removeEventListener("resize", updateCanvasSize);
+      window.removeEventListener("resize", handleUpdate);
+    };
   }, []);
 
   const handleUpdate = () => {
+    console.log("Update Canvas");
     shader?.quad();
+  };
+
+  useEffect(() => {
+    handleUpdate();
+  }, [shader]);
+
+  const handleButtonClick = () => {
+    console.log("Button Click");
   };
 
   return (
     <main>
-      <canvas className="fixed top-0 left-0" ref={canvasRef} />
-      <button onClick={handleUpdate}>Update</button>
+      <canvas
+        className="block absolute top-0 left-0 right-0 bottom-0"
+        ref={canvasRef}
+      />
+      <div className="absolute top-0 left-0 right-0 flex flex-col justify-center">
+        <NavbarComponent />
+        <div className="container text-center"></div>
+      </div>
     </main>
   );
 };
---
+++ src/components/NavbarComponent.tsx
@@ -0,0 +1,44 @@
+import { FunctionComponent } from "react";
+import Link from "next/link";
+import {} from "@heroicons/react/outline";
+
+interface NavbarComponentProps {}
+
+const NavbarComponent: FunctionComponent<NavbarComponentProps> = () => {
+  return (
+    <div className="text-white bg-gray-900 p-3">
+      <div className="container mx-2 lg:mx-auto">
+        <div className="flex justify-between">
+          <span>
+            <Link passHref href="/">
+              <div className="btn btn-effect flex align-middle items-center py-1">
+                <h1 className="text-xl">Mandelbrot set visualization</h1>
+              </div>
+            </Link>
+          </span>
+          <div className="flex gap-x-3">
+            <span className="inline-block">
+              <Link
+                passHref
+                href="https://github.com/SushiWaUmai/Mandelbrot-Visualization"
+              >
+                <div className="btn btn-effect flex align-middle items-center py-1">
+                  <h1 className="text-bold">Github</h1>
+                </div>
+              </Link>
+            </span>
+            <span className="inline-block">
+              <Link passHref href="/about">
+                <div className="btn btn-effect flex align-middle items-center py-1">
+                  <h1 className="text-bold">About</h1>
+                </div>
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarComponent;
