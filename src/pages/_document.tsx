6daa6886cd821581971884ee02f8d897cd519b60
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-10-16 19:54:09 +0200

    fix: :bug: remove prefix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { prefix } from "../utils/prefix";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="manifest" href={`${prefix}/manifest.json`} />
-          <link rel="apple-touch-icon" href={`${prefix}/icons/apple-icon-180.png`}></link>
+          <link rel="manifest" href="manifest.json" />
+          <link rel="apple-touch-icon" href="icons/apple-icon-180.png" />
           <meta name="theme-color" content="#fc2403" />
         </Head>
         <body>
