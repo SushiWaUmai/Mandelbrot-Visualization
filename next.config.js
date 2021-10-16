38a8cb7a57f09e6bd98b23e4ed7e1d091cac8e6a
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-10-16 21:35:08 +0200

    fix :bug: update pwa dest

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ module.exports = withPWA({
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  pwa : {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }
 });
