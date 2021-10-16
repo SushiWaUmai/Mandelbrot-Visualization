154c6c360d19aeb4a4fe339d739d7a8f67ff6092
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-10-16 20:07:52 +0200

    chore: :construction: update next config and manifest

--- next.config.js
+++ next.config.js
@@ -5,9 +5,4 @@ module.exports = withPWA({
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  pwa: {
-    dest: "build",
-    register: true,
-    skipWaiting: true,
-  },
 });
