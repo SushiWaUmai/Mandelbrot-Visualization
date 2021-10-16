64a97af6a14d08a7d74f346d3ade821541c2b34a
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-10-16 20:43:39 +0000

    Deploying to gh-pages from @ SushiWaUmai/Mandelbrot-Visualization@db34f78ccb82d5c60a255b883f7c817f540001dc 🚀

---
+++ _next/static/T_AIxLoXmEHV1B7s2JS0l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/831-d950f4c163c520ef72d1.js","static/chunks/pages/index-e2eafc15d510262bfbcc.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
